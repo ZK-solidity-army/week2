b468a74bae243e459a27a6f4e957aebb2d9f8ab7
German Kosach <fuerstein@yandex.ru>
2024-03-23 16:43:08 +0300

    feat(scripts): tested and fixed all scripts

--- scripts/viem/CastVote.ts
+++ scripts/viem/CastVote.ts
@@ -11,16 +11,18 @@ async function main() {
     if (!parameters || parameters.length < 2)
         throw new Error("Parameters not provided");
     const contractAddress = parameters[0] as `0x${string}`;
-    if (!contractAddress) throw new Error("Contract address not provided");
+    if (!contractAddress)
+        throw new Error("Contract address not provided");
     if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
         throw new Error("Invalid contract address");
     const proposalIndex = parameters[1];
-    if (isNaN(Number(proposalIndex))) throw new Error("Invalid proposal index");
+    if (isNaN(Number(proposalIndex)))
+        throw new Error("Invalid proposal index");
 
     //ATTACH CONTRACT AND CHECK SELECTED OPTION
     const publicClient = createPublicClient({
         chain: sepolia,
-        transport: http(`${connectionConfiguration.alchemyBaseUri}/${viemConfiguration.PRIVATE_KEY}`),
+        transport: http(`${connectionConfiguration.alchemyBaseUri}/${viemConfiguration.ALCHEMY_API_KEY}`),
     });
     console.log("Proposal selected: ");
     const proposal = (await publicClient.readContract({
@@ -43,7 +45,10 @@ async function main() {
     stdin.addListener("data", async function (d) {
         if (d.toString().trim().toLowerCase() != "n") {
             const hash = await voter.writeContract({
-                address: contractAddress, abi, functionName: "vote", args: [BigInt(proposalIndex)],
+                address: contractAddress,
+                abi,
+                functionName: "vote",
+                args: [BigInt(proposalIndex)],
             });
             console.log("Transaction hash:", hash);
             console.log("Waiting for confirmations...");
